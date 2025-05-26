e93a7407e2e28ae6c94679269717a658275f0c5c
Nicole-West <147952924+Nicole-West@users.noreply.github.com>
2025-05-26 17:57:37 +0300

    db.js

--- db.js
+++ db.js
@@ -2,18 +2,14 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  // database: 'Версия_8_5'
-  database: 'Университет'
+  host: 'bsmfi0igp7rbisdownaq-mysql.services.clever-cloud.com',
+  port: 3306,
+  user: 'ur7oz21mkjnslaht',
+  password: 'nQew5jeK6lLE9x8pmr8L',
+  database: 'bsmfi0igp7rbisdownaq',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
-
-
-// example1@example.com
-// suvorova.tt@example.ru
-// password1
-
-// admin@example.com
